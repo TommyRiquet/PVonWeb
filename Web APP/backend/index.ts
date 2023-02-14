9d2004e85b11e5b3bb702d3788763f511d91e7ab
TommyRiquet <tommy.riquet@hotmail.com>
2023-02-14 23:03:41 +0100

    feat(auth): Backend Authentification cookie
    Adding two routes for backend authentification /login and /register
    Also adding the whole cookie authentification method using argon2 and
    cookie-parser with the secret stored in the .env file

--- Web APP/backend/index.ts
+++ Web APP/backend/index.ts
@@ -1,21 +1,30 @@
 // This is the entry point for the backend
 
+import exp from "constants";
+
 // Importing the modules
 const http = require("http");
+require("dotenv").config();
+const PORT = process.env.PORT || 3001;
 
 const express = require("express");
 const app = express();
+
+//connexion to db
+const db = require("./database/db");
+db.connect();
 
 const server = http.createServer(app);
 const socketIO = require("socket.io");
 
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, (err: any) => {
-  if (err) console.log(err);
-  console.log("Server running on Port ", PORT);
-});
+var cookieParser = require("cookie-parser");
+app.use(
+  cookieParser(
+    process.env.COOKIES_SECRET_KEY
+  )
+);
 
 // Socket.io
 const io = socketIO(server, {
@@ -32,12 +41,21 @@ io.on("connection", (socket: any) => {
   socket.on("disconnect", (reason: String) => {
     console.log(reason + ": ", socket.id);
   });
-
 });
+
+app.use(express.json());
+
+const auth = require("./routes/auth");
+app.use("/api/auth", auth);
 
 // Path: frontend\src\index.tsx
 // This is the entry point for the frontend
 app.use(express.static(__dirname + "/build"));
 app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+server.listen(PORT, (err: any) => {
+  if (err) console.log(err);
+  console.log("Server running on Port ", PORT);
 });
