7d626598109e777e29fea97dbd2d69d18c2e740a
TommyRiquet <tommy.riquet@hotmail.com>
2023-02-18 21:24:04 +0100

    fix(deploy): axios skipping

--- Web APP/frontend/src/contexts/AuthContext.tsx
+++ Web APP/frontend/src/contexts/AuthContext.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import axios from 'axios'
 import config from '../config.json'
 
 
@@ -14,27 +13,27 @@ const AuthContext = React.createContext<AuthContextType>(null!)
 export function AuthContextProvider({ children }: { children: React.ReactNode }) {
 
 	const login = (email: string, password: string, onSuccess: (res: any) => any, onError: (err: any) => any) => {
-		axios.post<{ key: string }>(
-			config.API_URL + 'auth/login',
-			{
-				email: email,
-				password: password
-			}
-		).then((res) => {
-			onSuccess(res)
-		}).catch((err) => {
-			onError(err)
-		})
+		// axios.post<{ key: string }>(
+		// 	config.API_URL + 'auth/login',
+		// 	{
+		// 		email: email,
+		// 		password: password
+		// 	}
+		// ).then((res) => {
+		// 	onSuccess(res)
+		// }).catch((err) => {
+		// 	onError(err)
+		// })
 	}
 
 	const logout = (onSuccess: (res: any) => any, onError: (err: any) => any) => {
-		axios.post<{ key: string }>(
-			config.API_URL + 'auth/logout'
-		).then((res) => {
-			onSuccess(res)
-		}).catch((err) => {
-			onError(err)
-		})
+		// axios.post<{ key: string }>(
+		// 	config.API_URL + 'auth/logout'
+		// ).then((res) => {
+		// 	onSuccess(res)
+		// }).catch((err) => {
+		// 	onError(err)
+		// })
 	}
 
 	const value = { login, logout }
