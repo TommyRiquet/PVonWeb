10629a345e80286137b6eb8ca8172f84c42e7e72
TommyRiquet <tommy.riquet@hotmail.com>
2023-04-08 12:14:32 +0200

    feat(token): user generation token

--- Web App/backend/services/authService.ts
+++ Web App/backend/services/authService.ts
@@ -2,18 +2,18 @@ const jwt = require('jsonwebtoken')
 
 const secret = process.env.JWT_SECRET as string
 
-function generateToken(test: string): string {
+function generateToken(id: number, name: string, email: string): string {
   const payload = {
-    id: '1',
-    username: 'test',
-    email: 'test'
+    id: id,
+    name: name,
+    email: email
   }
 
-  return jwt.sign(payload, secret, { expiresIn: '1h' })
+  return jwt.sign(payload, secret, { expiresIn: '24h' })
 }
 
-function verifyToken(token: string): { id: number, username: string, email: string } {
-  return jwt.verify(token, secret) as { id: number, username: string, email: string }
+function verifyToken(token: string): { id: number, name: string, email: string } {
+  return jwt.verify(token, secret) as { id: number, name: string, email: string }
 }
 
 export {
