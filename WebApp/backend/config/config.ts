1095ae82d35a51d80344619a48238c2cb92bcb3d
TommyRiquet <tommy.riquet@hotmail.com>
2023-04-09 11:52:21 +0200

    ci(deploy): ec2 deploiment script

--- WebApp/backend/config/config.ts
+++ WebApp/backend/config/config.ts
@@ -2,29 +2,29 @@ require('dotenv').config()
 
 export default {
   development: {
-    database: process.env.DB_NAME || 'pvonweb',
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    host: process.env.DB_HOSTNAME || 'localhost',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
     port: parseInt(process.env.DB_PORT as string) || 3306,
     dialect: 'mysql',
     logging: false
   },
   test: {
-    database: process.env.DB_NAME || 'my_app_test',
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    host: process.env.DB_HOSTNAME || 'localhost',
-    port: parseInt(process.env.DB_PORT as string) || 3306,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
+    port: parseInt(process.env.DB_PORT as string),
     dialect: 'mysql',
     logging: false
   },
   production: {
-    database: process.env.DB_NAME || 'my_app_prod',
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    host: process.env.DB_HOSTNAME || 'localhost',
-    port: parseInt(process.env.DB_PORT as string) || 3306,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
+    port: parseInt(process.env.DB_PORT as string),
     dialect: 'mysql',
     logging: false
   },
