f4df9d9efd6ac8b4add48f1e828706ec99bef917
TommyRiquet <tommy.riquet@hotmail.com>
2023-04-10 18:05:08 +0200

    feat(team): members search close #64

--- WebApp/frontend/src/components/app/views/Team/TeamMembersListView.tsx
+++ WebApp/frontend/src/components/app/views/Team/TeamMembersListView.tsx
@@ -4,7 +4,7 @@ import { Box, Typography, Button, Tooltip, TextField } from '@mui/material'
 import LogoutIcon from '@mui/icons-material/Logout'
 import EditIcon from '@mui/icons-material/Edit'
 
-import { ListView } from 'components/common'
+import { ListView, Loading } from 'components/common'
 
 import { useTeamAPI } from 'services/teams.services'
 
@@ -62,16 +62,41 @@ const TeamMembersListView: React.FC = () => {
 	const { getListMembers } = useTeamAPI()
 
 	const [listMembers, setListMembers] = useState<Array<any>>([])
+	const [searchText, setSearchText] = useState<string | null>(null)
+
+	const filteredMembers = useMemo(() => {
+		if (!searchText) {
+			return listMembers
+		}
+
+		const regex = new RegExp(searchText, 'i')
+
+		return listMembers.filter((member) => {
+			const name = `${member.firstName} ${member.lastName}`
+			return regex.test(name) || regex.test(member.email) || regex.test(member.role)
+		}).sort((a, b) => a.firstName.localeCompare(b.firstname))
+
+	}, [searchText, listMembers])
 
 	useMemo(() => {
 		getListMembers().then(res => setListMembers(res))
 	}, [])
 
+	if (listMembers.length === 0) {
+		return <Loading/>
+	}
+
 	return (
 		<Box display='flex' flexDirection='column' width='100%' paddingX={5}>
 			<Box display='flex' justifyContent='space-between' paddingY={3}>
 				<Box>
-					<TextField label='Search' size='small'/>
+					<TextField
+						placeholder='Search'
+						size='small'
+						autoComplete='off'
+						onChange={(event: any) => {
+							setSearchText(event.target.value.toLowerCase())
+						}}/>
 				</Box>
 				<Box>
 					<Button variant='contained' color='primary' onClick={() => console.log('/members/add')}>
@@ -79,7 +104,7 @@ const TeamMembersListView: React.FC = () => {
 					</Button>
 				</Box>
 			</Box>
-			<ListView columns={columns} rows={listMembers}/>
+			<ListView columns={columns} rows={filteredMembers}/>
 		</Box>
 	)
 }
