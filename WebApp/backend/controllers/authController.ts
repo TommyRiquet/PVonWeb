f325019713fbdf8bf349fa6625f9d19f47e54bd1
TommyRiquet <tommy.riquet@hotmail.com>
2023-04-16 15:03:31 +0200

    feat(token): fake token verification

--- WebApp/backend/controllers/authController.ts
+++ WebApp/backend/controllers/authController.ts
@@ -2,7 +2,7 @@
 import { User } from '../entity'
 import { AppDataSource } from '../config/database'
 import { Request, Response } from 'express'
-import { generateToken } from '../services/authService'
+import { generateToken, verifyToken} from '../services/authService'
 
 export const login = async (req: Request, res: Response) => {
   try {
@@ -26,11 +26,27 @@ export const login = async (req: Request, res: Response) => {
       return res.status(401).json({ message: 'Invalid credentials' })
     }
 
-    const token = generateToken(user.id, user.firstName+user.lastName, user.email)
+    const token = generateToken(user.id)
     res.json({ token })
 
   } catch (error) {
     console.error(error)
     res.status(500).json({ message: 'Internal server error' })
   }
+}
+
+
+export const verify = async (req: Request, res: Response) => {
+	try {
+		const token = req.body.token
+		const payload = verifyToken(token)
+		if (payload) {
+		res.status(200).json({ message: 'Valid token' })
+		} else {
+		res.status(401).json({ message: 'Invalid token' })
+		}
+	} catch (error) {
+		console.error(error)
+		res.status(500).json({ message: 'Internal server error' })
+	}
 }
