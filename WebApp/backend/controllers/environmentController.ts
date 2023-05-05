e586dcc7fa0d42d2334c24cb4941b40357e549ba
TommyRiquet <tommy.riquet@hotmail.com>
2023-05-05 16:23:12 +0200

    feat(environment): change name
    close #105

--- WebApp/backend/controllers/environmentController.ts
+++ WebApp/backend/controllers/environmentController.ts
@@ -7,6 +7,67 @@ import { User, Environment, Transcript } from '../entity'
 import { verifyToken } from '../services/authService'
 
 
+export const getEnvironment = async (req: Request, res: Response) => {
+	const userRepository = AppDataSource.getRepository(User)
+	const environmentRepository = AppDataSource.getRepository(Environment)
+
+	try {
+
+		const id = verifyToken(req.headers.authorization.split(' ')[1]).id
+
+		const user = await userRepository.findOneBy({
+			id: id
+		})
+		const environments = await environmentRepository.findOne({
+			where: {
+				users: user
+			}
+		})
+
+		res.json(environments)
+	}
+	catch (error) {
+		res.status(500).json(error)
+	}
+}
+
+export const updateEnvironment = async (req: Request, res: Response) => {
+	const environmentRepository = AppDataSource.getRepository(Environment)
+	const userRepository = AppDataSource.getRepository(User)
+
+	try {
+
+		const id = verifyToken(req.headers.authorization.split(' ')[1]).id
+
+		const user = await userRepository.findOneBy({
+			id: id
+		})
+
+		if(user.role !== 'admin') {
+			res.status(403).json({ message: 'You are not allowed to update this environment' })
+		}
+
+		const environment = await environmentRepository.findOne({
+			where: {
+				users: user
+			}
+		})
+
+		environment.name = req.body.name
+		environment.description = req.body.description
+
+		await environmentRepository.save(environment)
+
+		res.json(environment)
+	}
+	catch (error) {
+		res.status(500).json(error)
+	}
+}
+
+
+
+
 export const getStatisticsByEnvironment = async (req: Request, res: Response) => {
 	const userRepository = AppDataSource.getRepository(User)
 	const environmentRepository = AppDataSource.getRepository(Environment)
--- WebApp/backend/routes/environmentRoutes.ts
+++ WebApp/backend/routes/environmentRoutes.ts
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const { getStatisticsByEnvironment } = require('../controllers/environmentController')
+const { getStatisticsByEnvironment, getEnvironment, updateEnvironment } = require('../controllers/environmentController')
 const { getLogs } = require('../controllers/logsController')
+
+router.get('/', getEnvironment)
 
 router.get('/statistics', getStatisticsByEnvironment)
 
 router.get('/history', getLogs)
+
+router.patch('/', updateEnvironment)
 
 export default router
