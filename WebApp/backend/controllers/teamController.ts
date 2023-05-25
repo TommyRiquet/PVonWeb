5edd4745281bf6996dc04674da93d212fbe4853c
TommyRiquet <tommy.riquet@hotmail.com>
2023-05-25 22:03:38 +0200

    fix(team): role display
    close #127

--- WebApp/backend/controllers/teamController.ts
+++ WebApp/backend/controllers/teamController.ts
@@ -14,14 +14,33 @@ export const getTeamMembersByEnvironment = async (req: Request, res: Response) =
 
 		const { environment } = await getUserAndEnvironment(req)
 
-		const teamMembers = await userRepository.find({
-			select: ['id', 'firstName', 'lastName', 'email', 'phoneNumber'],
+		let teamMembers = await userRepository.find({
 			where: {
 				userEnvironments: {
 					environment: environment
 				}
+			},
+			relations: {
+				userEnvironments: {
+					environment: true
+				}
 			}
 		})
+
+		teamMembers = teamMembers.map(teamMember => {
+			const userEnvironment = teamMember.userEnvironments.find(userEnv => userEnv.environment.id === environment.id)
+			return {
+				id: teamMember.id,
+				firstName: teamMember.firstName,
+				lastName: teamMember.lastName,
+				email: teamMember.email,
+				phoneNumber: teamMember.phoneNumber,
+				password: undefined,
+				userEnvironments: undefined,
+				role: userEnvironment.role
+			}
+		})
+
 		res.json(teamMembers)
 	}
 	catch (error) {
