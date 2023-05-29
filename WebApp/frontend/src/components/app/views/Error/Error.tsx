222a169311af3f91e5d30fbfcf5029b345137eeb
TommyRiquet <tommy.riquet@hotmail.com>
2023-05-29 17:28:34 +0200

    feat: 404 page
    close #134

--- WebApp/frontend/src/components/app/views/Error/Error.tsx
+++ WebApp/frontend/src/components/app/views/Error/Error.tsx
@@ -1,7 +1,19 @@
-// Description: Error page when no route has been found
+import { useTranslation } from 'react-i18next'
 
-function Error() {
-	return <div>Error</div>
+import { Box, Typography, Button } from '@mui/material'
+
+const Error = () =>  {
+
+	const { t } = useTranslation()
+
+	return (
+		<Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' width='100%'>
+			<Typography variant='h1' sx={{textAlign: 'center'}}>404</Typography>
+			<Typography variant='h2' sx={{textAlign: 'center'}}>{t('Page not found')}</Typography>
+			<Typography variant='body1' sx={{marginTop: 2, textAlign: 'center'}}>{t('It seems like you are trying to access a page that does not exist.')}</Typography>
+			<Button variant='contained' color='primary' href='/' sx={{marginTop: 2}}>{t('Go back to dashboard')}</Button>
+		</Box>
+	)
 }
 
 export default Error
