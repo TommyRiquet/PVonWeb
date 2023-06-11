3055a02d7ed6b1a524e4595b2091cfc08ab427c1
TommyRiquet <tommy.riquet@hotmail.com>
2023-06-11 14:20:39 +0200

    ci(env): production env

---
+++ WebApp/frontend/src/components/common/__test__/PrivacyPolicy.test.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import PrivacyPolicy from '../PrivacyPolicy'
+
+test('render PrivacyPolicy Component', () => {
+	render(<PrivacyPolicy/>)
+})
